2ab2f1bb88fda6d24c6c17138ac5b6175e1fbc1a
msancheznegrette <msancheznegrette@frba.utn.edu.ar>
2021-05-31 16:56:55 -0300

    prueba

--- worker/src/main/java/worker/Worker.java
+++ worker/src/main/java/worker/Worker.java
@@ -4,6 +4,7 @@ import redis.clients.jedis.Jedis;
 import redis.clients.jedis.exceptions.JedisConnectionException;
 import java.sql.*;
 import org.json.JSONObject;
+import java.util.HashMap;
 
 class Worker {
   public static void main(String[] args) {
